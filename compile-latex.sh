e24aa707c2b7c3aa3588a222aed55b885cfa65c3
Javier Clavijo <jjclavijo@gmail.com>
2019-09-07 10:06:55 -0300

    Actualizado compilers, Recompilado htm.
    Se modificaron los permisos de los compilers, y se agregó
    un fix por el paquete xcolor, que tiene que ver con el template
    de pandoc para latex.
    
    En un futuro deberiamos mantener un template separado.

--- compile-latex.sh
+++ compile-latex.sh
@@ -7,5 +7,5 @@ foot='\\end{minipage}}}'
 
 awk 'BEGIN{cuadro=0};/^----$/{if (cuadro == 0) {cuadro=1; print "iniciocuadro"}else{cuadro=0; print "fincuadro"}}; $0 !~ /^----$/' $1 |\
        	pandoc -t latex -s --pdf-engine xelatex |\
-	sed 's,.*iniciocuadro.*,'"$head"',;s,.*fincuadro.*,'"$foot"',' |\
+	sed '/usepackage{xcolor}/d;s,.*iniciocuadro.*,'"$head"',;s,.*fincuadro.*,'"$foot"',' |\
 	xelatex -jobname="${1%.md}" -output-directory='./pdf'
